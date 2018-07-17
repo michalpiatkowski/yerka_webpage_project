9ded3d669de7877ac78d60a82be8acd84efd5c0d
michalpiatkowski <michal5piatkowski@gmail.com>
2018-07-17 11:33:29 +0200

    add contact.html

---
+++ js/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', function(){
+    ReactDOM.render(
+        <App/>,
+        document.getElementById('app')
+    );
+});
---
+++ js/main.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from "./components/app.jsx"
+require("../scss/main.scss")
+
+class Navigation extends React.Component {
+    render() {
+        return
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return
+    }
+}
+
+class News extends React.Component {
+    render() {
+        return
+    }
+}
+
+class Gallery extends React.Component {
+    render() {
+        return
+    }
+}
+
+class Footer extends React.Component {
+    render() {
+        return
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    ReactDOM.render(
+        <App/>,
+        document.getElementById('app')
+    );
+});
