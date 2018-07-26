757c56a5e9d5a2cde9e9315cd9bcae60154bffe4
michalpiatkowski <michal5piatkowski@gmail.com>
2018-07-26 21:24:52 +0200

    add new paintings to the paintings gallery

--- js/components/works/paintings.jsx
+++ js/components/works/paintings.jsx
@@ -13,77 +13,92 @@ class Paintings extends React.Component {
         <div className="paintings-images">
                 <div className="pastels-gallery">
                     <div>
-                        <img src="./img/paintings/last_minute-senna_oferta.jpg" />
+                        <img src="https://lh3.googleusercontent.com/eC0YbNvhAwbIFqRFbSQAJa8GZU9D3aBu7wBLoJpCbYWU0OvDOSiiGp-F08sDZr71u5FJCEBHT8nphrjP4mKGyBr5ubriIzWyhvHrRy3V2w2ISKu_RMIs8SqsbFyOH4puztspOjkcBRW1Wpfh3wYUFcJg6jjG51eqHcF8wDJ9-GoXeuwGWgJHvLZ_Ov39VnHcARQEQ3Ig5B8fIPIb1f4hm_Z0xKuXGvgYL4v3BnZ8t-hUknF51YO2CLB9jd1pTVTmXW_uTzqgzJM6_VnlYUDgDoU3G7GSQs45T7ywqRefYDDjc1MbmUOl32zvVMrROMZZsXvjBXZVLBOho6bEYK5N6VVpSMZ72BkuNUsshz7kkM6KYAAyXROxZTFEUjvAT6McTzFdU8k6DDCy2V168kEfc_oG1yVaaQJdtop6ugqqvp4fLkIesNMguodO62H43bGxCukCmQqco9vWt_xlZ-qi46hvlIJ9aTlQ_wzTqUpovI6wqGlogV-MPk6ysTmbgLjKmpdHloGUcOKorfNPx33Hii0oqlNqghTuR-860vBpYGv445LJG1xiYQMh7rantH91zRZ5ccSmM85T6Lk-5h4XXkdQk7HvTO26BUzRoN3wYXDw95QagolPcMAWwTR8PQzKdSKpDFy3hZjd29vCy19o1MnGbqUEKlYp=w611-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/lato_w_miescie.jpg" />
+                        <img src="https://lh3.googleusercontent.com/c-LprPzn3iJ8E-jf4gkeCMzB1dO8eYrgOsr_76LFOT1qV90Czr-gy_w5XB-wjAUHKZEnxSzQQRc_6WO9iSLHZO1pASeyXarI6s4KX6IM7QC-ubeBw7MdFbRnQivzZKUt5qZarVLnYR8sT9_c1UfnXF0QlW0taXRZzZL-o7-6TlH9NK56IR6mt6RemJox2_6xRnZ-uQYPvj5Rjf8pIo0KpyEwaUZnbTZg0VUIa2QS6Eb5R69RJygUsPjkMlKv44eroM0Y2Wf_gTnJTOiyVEe2D5y8dhGVUkUVZSzrMjH7HdwL1wObJsIam81dTWICQJIA7kuRBJOmSkz445Zy1bvmvjsyySjkbtlYmKNzmR0R4IBExrWuB42VuwUHYLH5KekhMyw68Es3Z5K-4WZVdap1gTs4hPyDml0sHZmG41zVEE2OLp9DUbTbCaZmSwkzM3sb-GCKUdmmkc4b4ZAEgzU-aDl5lIP5La7o3tNRffvE3Lf1RT93M1DCNZhxXA7tegp4XG4TbqCIr9iaK1O9jw73q_HKApx5bkhiazyeWDiAbMpRD4O5Ne5405uOZAV_07HkkXxC-0jA89C2NPPwCZprh05MIr1FimuyUCuPkp__zbUeTTzREQSrhtHKmUb1s990cBbo18kdgANYE-yG-MwNk4HqZoCNwxE1=w404-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/lazienka_oceaniczna.jpg" />
+                        <img src="https://lh3.googleusercontent.com/owVptngiSKrloyKtLYpY1pKb6Zgn3zQxtzC2ADTUkCvgZFGTC9regzTzppUOt3W9l44qg5KtiyI6DIWMWZjAr-Gj0c6-JJRvv1DHBNUfk5_2xvLU3c7YTRdfkeE_fxfDiZumVcmfheRK-fx0sCfBlJZtif6fOD2drJDc3Nf3UliJW66jUTwzXjhvbUyv4OxhPzSZ_0g69ufQBQ5HAOwYMn3oQ7Z6ILdqxGx3c_0UzVkZLXsNgXy_aZ5YEkqJdeFJ3zyeE8Orba0wTMgjseFZOemgwOllfXxbxi9csHKqhUOEGTA2v0Nhzz9Qekemjs-IrFRobHz2P7s7yJAcK0AwmP-ZzL4bG-3GBAwK12QY3fk2fpzasJz_8fNwgA0mrOBRLqRLciii8b_EEvIVgylB3wriDeaNJ5fDaolqJ4ZKQWEo6zhT79Q2jHrckhsTmJHMtwj912XVVsdbL_KQubs5epsyDUOBxLf16H1lMchs_TUv5x5DiHN7Opyb7jji_flSuIYtxJ8UzQp9MLAlDSayRD0wBQgSHePrA6AoWRrqzs2NMypW0cea1-yUWMZFXWcU7iTPqP88V_I21C2_3C0a4HqyJe6IrqG-516lJNi0wJMmzgj4Cq_ESFbjWL3u6diVLDOlwJ__GwdBPSl6fNLA5fDILax6jvpf=w556-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/ladowanie_miasta.jpg" />
+                        <img src="https://lh3.googleusercontent.com/vi4VAWD_D9lMA-JbT0xPLm1-uITiEoF50nrL-tcTt--xfR-toysBZYzuGe57m3cmeKPMCwRBhhvc0Hdw_SiUc9eN1Cq7lNu7oIwlL7b9iCed0hNPOf0op287yYvKlYdT8Rp_7ceP9tQHaWV3Sbl5lBXjxZS-Ns_96RBIevUTKfaU1LWPGyoIrUxn9c7neiK7HlYXy9YXnxpd61buSxI1_k5wse3ihewDdAdqyEVuDI-RESezXAnxErKoQkvzwze8Dli22-wp2u3UT2qqPeMB0jMJPnIVleEAx4JG2-7kwQkgRlr5T2u29nL8kYML8z3UGHRoGM9-kOh40VsJ9CRvgZv4Ctwl162pCR22RjrId4fYBrRM6fy7QhtcK_0QWYMNCO8mMcrCi8fPC9m8YU29dNpTHyvasAeSL00sVnVGozj2cvIYCA_mEKz49iy747WmdFg9mLMGLJ3KrvyZORE_r92VdzxUuwwQinswb3t_lvw6oy-XnPSLMSemw-ySopVD3VeX6dk_yMs0_6lKxGA-6Axu4LsaHyIIzfDGudyiS2mdCTvTCd1oHwsj54s9dMkUvv_E8b1lbGQx3nzw3ZgbAnpHQRfBVbzMKNdXEll3cbmkBC5kVP1gaB8ppM8bQfNxgczg2egOobgRN3l__7O10AeTdP0V9I_i=w443-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/letnia_sypialnia.jpg" />
+                        <img src="https://lh3.googleusercontent.com/a1kJliosF-cQFqZ8lv1_fegDAEFj9mQV8guLC3ye-C2VNBB4UYBaj8SYh_25Eh0RddbsZnhqI7FBNPENwB47l3EvJ9_HJXogFhdnDHVKyjREQwn4h8h1dyG69M6s9sfzbyasnFsyptXZUe6sKmAwP8ppLmSZG_sBYRZgvtEXRFP_XhahQfWEAtSA012yrRSulcfApWbiJZOIe9TS1v2F1zaoP--RESxrTQr9mD53aVE55-u-gSGw7EpA_6E3wqaoQnVmiFq-mbW-mKgSVHLf3X1EbnRKdRkIhVGsi2I8RmqOFCM1n7kygaERwOiGNb3woTdwa9y_AimNZtP0LPFLGyBGcVBKTMDCkUpkMYq-CQgNFYJptQEvryD42Vqm2sniQ-B7BvZtai7tFkVCXRXR1CCnCRmOM3y3jK5-K3HFwdckaKfLVemR-0pllRiFZVCFh5lA3sRlnx2tjn0IzWo8IiTbyf2IefflD1YDw0zlK1LGNH7NvZJZpaE6jCbX3_oCGS5bp_cv7FTtg4g5TGX4_NAHxjGxIwwpr4Zit7m6sqDaKa_7__BfdHuky3LIl5gHm5O84zPAlda1aT76hsGiW4TKz1_l-IXQ9mwL4_c4SQwPh6ZHRdpoi4ZZLfxlUQhvMAd-5uQ63w25plS0ZVYgMXJAlJGc0dGE=w559-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/my_ryby.jpg" />
+                        <img src="https://lh3.googleusercontent.com/xVZ8Fuu6Y5pCg8nOX_2N_kn7935NourUchY51_WYW5rBBrrDgTFuhfmaJ3ZcEUF5R_zdzGeezgEcMvMpUY_O761slIGiofbPN4uS1E8d2HEGqv4dNc68mOOoN6vUTg6h67x_EE7z1SOiyEh8hyI7a6nDHghFmT-PBhoXHWSBLCJ6EEjaj9Q4fvUVYLbV53eYD3BH1ZWBs-SFpV9yKTCS-fsthH-1-s8vP065fbPmagos3XcykK9WLkFP3WVaB_SgaIemUXoNTSqzLiufb5VXVxrHZSEBhOK-qz0uUExwmxD45StbEW02rO6dy5go-U75dIir2GRP9HyRSkxUmAGz01rXLtvZAy9-_l0EsJucvgF8MgXyJSrrt_MDsalGUZDTo1zlP67WL5d2dKRr4Wrt25vF1Ui-vwI6SgMxVjXyDnY0Z3pBdVyUoMrIJhMU2BKFmAOJZymm8DsVtxTUJdy0mqRJxVjj_mYH5IQiIcYVWiETkBlghjyJaJFTnoT0tkXMevE8nfjUbPM6uAo3b6qzjA4SBhzNl3N_y0U8oOnD8Dp2WYOMCfIXxAYbQ-3wPgN6M0_h-hH9U55H_E6W3JPGnI1HmVMG2MD1bOGGrEFqK239xJlEx9zmMdPIS6dW5KileFRnWaNd3jE1cwZ8O5aZlXZFhFUEowYc=w563-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/narodziny_zycia-66.jpg" />
+                        <img src="https://lh3.googleusercontent.com/g0ASC1S_mAvko0jT22sR9svhOhdEpmcvt1Mfx1vrRsA9m-k7m1xszAJpLMcnCsuinsAOZ3J7xknWbhxaURyIaoVDLRn_2gsh_BextlCPQg9if8GyIaqk6hbIuZk4P6Zrf-mZ75TXuwvCOYEbg0eAAfED02Jhxk9jpg2q9TEEbUpSzzNm7ELrDrq2xD1_LHdE_ggh4dPM6oEm8SO_TFF-RkmRfyQsJss7N96iq1GKy_3AlS5Uocdp5PaY4uFy2cbjIdY85T8A-tPykKww9D-KfjPSAsdLW_zjm6NNFOnum9g8idnbFrfaGtD1yWJuAPjKzD5EXAhylwha_aZTk5qlUttkzmsvdohhQtSKoIkbvfz7H4OVYBvyEmqSjmM4lVCOcoTcd6UYosdY-I5f7ZYfoYTXvdQEHyU-gmys5ZasW_Syw891hrOL3fO6QaIRhjsQlEoXY-0v_n3kk5KVspGeK9ykPisiDE9o94cjxPdaV4cZVba_Tnc8w-FK1wPFSuCa_7Rr4mBlygiHAtTorBp0fCL24jk_1R14BJGXf8s1t6AZ94XMZKOK1G1RrhdFldmasD1lHdqORpU27LlNOQAcmy_5jRfPeOQ4PYhOI0WMnWzpjiFtMoL64ELIMyh2hkqZWv0pOgiVbBK4VWNIS7rVMVVdRA552xYc=w450-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/nauka_chodzenia.jpg" />
+                        <img src="https://lh3.googleusercontent.com/ZwNxva7gcQn6F-ia6AL8W09J6-0BfsgLCLqFQCRvXe800bHbUkjxD5wBtA0aa50tzgCIIMNuCdkcqCVwKOY9Vg6jbIDphxw-Rs7oXdfYL57H80nmc2nBm4pAqOJDSnS21kOgG6xV3S_t_EIDGN4xjSrSLTMl6TGf4qHusBLHjP4a1dUtlVMabU6UP8p6XHrjerv21CXrFJFmV--qYLctBOssxL1mf6k1YFIvRYRIY_dnj7MY7PE6N4qFO-j3wTMZCzpvdxcownNMRGktXOiB-ngPODE3v4_NjdWLaPi36rsSRFM1T4f8gGtsyv00aemKbSQDbt5q21sbbOJEuH3Ip-uEBFNUcxW_BD4sEC35Y_MIju_Ll-ubvwvBQaGBISC1LU9M1Pw-jA8QPL4CPkE9ujlHsLu1xRsCd66Ruwt5uRlpZh61puPnA-VT3E1GQz83Ug2y7UgAvAzX-w0R87LrdRelB_1FwHDffprppUl0YV3PHba7mvvdF28h2xaNmY9b1u4llEkWxAgKpf7ocHUkAFqVeVuzE9gUjnmK2QiCbJDiXc8RWmeOIv5PzFJeS1dGPIKWGe0IrXO91IZ0ctVVZOE2X0PihiSmTROTTZhRVMOmI41i4ilWYmkvk9TjrWtyrjipX4boNhNRBisE-X-QlHTGAQQfvJTf=w386-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/nie_trzaskac_drzwiami.jpg" />
+                        <img src="https://lh3.googleusercontent.com/YHCsFCir6mf9tpgoExDBv0vYHZUxXV9OHxFq9LMa9XsipcnfDtoNsl4Zu6hkovOXPZ0EBB2kEK_AFBF-JyQDq39s86Wt2AaWI7CJrovB9nay3cN4mJYLGtFQEwpAGE2vH2MjRPegtiXEQ5HLH68MrzgdMw0GPJJjXidCfvR4mg6dkttz8rj3CR2HgJf_2ZMxGdjO2GbVFjDjrjyOCpotuSXaXtOGH77oEyoCPuhV_cTcsbNuTHyoIeB2WB6LdiM-EwPQieEE32kwez7Fitn1HRznjBucb_JR-aP8s_-JzyeZUnO2IaF_59DNeSwOPPnLLs7lKmfVMRwLKu2anPk1xXnO26CaGRZUm9E0Zg_Imuyl02y1RehIyG5PInKTIabC-2eQ7qLukKSwbA56zgLt0r5-Az29VgtfyISmjU9jpPKtg-pXve8hRtTVuf0jZCkOBavCTzII2YfHtP9-ygeQWIjicaHlCJILBYVUW0FrMfSaKAN7boBHxKVhQWrAfllQTpXygJPpYJ9KqOm55Jgjsr7grRjnYOGFAsYpG6wzfSBNo2tUfwA1GBiKcFDKdMdPvmVTQdj01YoY2nVO_KyZz9K9aOD5-Voi1wEtH-6Ab0RjAjt4f7ggKNmA_-2bPrB7UA_IscuUhaF_mBxqRDYRpQWFcN4-iCh6=w561-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/odnalezienie_sw_graala.jpg" />
+                        <img src="https://lh3.googleusercontent.com/XppOkUr-dWgJW9iM9WZCAioAwL7PRDVhlAuY2oaK-j5REFCPhasU6mhM6liFe62-9gnDp161L_-_S9mHPO1Kjpr-NalrcHqzFeqW8kgx04O0lvGKQmRmAKG88T0sWSvzymkS0TQPFsph90zAzJ9A1sD5LJBLeDZj-z4sGCDDUWAG_4Vqy2SZibJzk3xW3kP5lzNleDBNYZY5DzPi0j4vDQCtr3uLzMdkStQR62kPW5oL18_3KrFL1UndlnP6xh762NLKJyimQJrXlmb8_pJ-4Y_x6PziVWU-bsZuakoOMIVDawTXq6WoJXK7MByuNWbLxgNRkiEolbO7yb6TqUGw9bmvyR83ziWBc472zJS1nsJcvW85vQZW1Dqis7xXi1gnhcouYGhJBOhIgOOJhZYskUXsQxj0sBKNL5OUlYQGmVfqXTvn16IicgGjC8FaP2R4RGNy8DiH0B3lAuXJ8pgqfonaWGPSU1ebmxb0fck2fVYakFxZs0ohfQ1MF6A_4uh_d6JErCI7ffRkUQag5XmBwkBqn307jXcplp56M2gdrKSm_C5ZkV5fYez6tRHFCO4EYvyolmEKrCB5pUUc2KhOZ-P9rZcDZcstO_J_Z13xKlo7vlIUm9Ai9l532xZVxTSsERFDaL2mfakSPQUZX9nou7pArwsjo2hd=w554-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/partia_na_rafie.jpg" />
+                        <img src="https://lh3.googleusercontent.com/sSxFQ33tT0eTFQAzMho96tm3Q6WzPTDAnAMjStZ809dUxlPnb-qxteKKvIgB2WjGbA-7_hszeb5b7Nv40DIlLb7Bpdds0UzUsKZKIyMe6mFDWzWfy006wOTMM10voQXH-NUfYRvrKpBKt467MwR9kyti6ICQ-3LU9RjLHDOJsI1gv_rQ7RUtA0qM_XYobyPYqnpBxiC6Wwuo14HNIluAgoMIjCXPLWUXGhIzvu5yfVjRyr0igEfsScZGaJtkPALxOnPP-4ZTdDD4Xf8TQnMP4hmoTM8Jvi0inlKMtaFNmv9jIOO0mumm9DIxvfpN8XbBy0Gd7UyH8Pg_WTmEYpSABgQ0baWElvzT0ena-5IK8fGCREk_M3GqJy_4v3KCx2wcn-Bx3oJuqSAdSj-kBLXLvHlkc7uBN-WF3VVGuaqqxoeqD9NsB570RaltbKcSkSMX1wan3o7Jzz3e-cn0mrCPIBcQKK00tiTdmhSCGSJJDHB7bOeHd5CSfEUA49ywk7Yru3YyGo5XH3MnbWfF17zZl9bV72K-3bCSL_UrwMyLV4R80U3I6chvRFfyXf_CoR8EFN1ffDll7wUZQPmqhFn-O6PMuDQZFqjiUzPmshZ33BoriiWC6z2Nw1sd5AOjSiPrdI04vFM7B9bUWimkzujRMnRbcPdr-Fvj=w569-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/partia_na_wyspie.jpg" />
+                        <img src="https://lh3.googleusercontent.com/valvPmQevkMVA7xVbvfTKdfvhEI99phfmDzI70ZgUFm39uRzQnCurmUqidt1qfc_GROQRHygxL_SQT1efIoorCK7yBsZF_wBI-ShdFVuRUWL26M0RDp5OdLBQJ8VeHOIXBMKcLzIwsATk2-GkC678HYG9zPAZ0F96LMpwhrCPAl2XDkv7J402LkLlxMrv3vfPtORTPzWdm3xriajLZ5rUSJmBehf1YjKWQUAA7-a1apytBpnezEWnhDUiJmLNHrc3XALuA-li17tzDP0WoMBufSeY2SmW8p9G7qYF22EB4Z2pFqQY17YSfCCN4u_3pG86NbD7LttLyYj9nv0MaCGo07F3i2Y1YLva7Edyx5jwKAwXIo2ieqhwdaJ3M-rXKUc-Eqj75R0yYU5-trCPL8fRu_mhwnz0u63bg4-e7UQ9r3XJne_CSB15VqjPOzOF-4OulnGJUuxNWDXvNn5_r8-r-SXD4s66QdfxGbK8GMrWfBrmS2p6WBI6NKf8YHuWNkFyMiNrYGUfLorbzcKicVz2_BYF5yrBvLuUzHDxQSFP2dqSTL0oBNK_U3FhCrKyxbrLXsWVOgUuez5_hqMs72HSgWAeDYm4qiKwES1A_SQr8CZaeZVD0Boq7N1wKG3tg7GvJFOCyrsbU4GMdM9eHizDtA5YCbM8CPU=w552-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/perlowa_zatoka66.jpg" />
+                        <img src="https://lh3.googleusercontent.com/Y94tstCjk0jqJ4pQI9cJpxq7t7pzgsrz3p1zqqpVt2jqMVeTLzeakUB72m2VbNtKNY4q9POuwIMFQjnMJXQO_0oar-KWiyh6vW89r_3R1u6ZhBAkCOb3PqKfX5S7QVxKVSZH4RfuOXFRl4J8fhM0C8068HrW_ViVpMzKtw3bPzEh1AVy3t-lr_MBJ7_enkm-YdjVpnsiS6xRi97xkILZtyY-9nx_wJDbL2-eqatLoXLlndNDtgg48THrbrqwV55uv4P2AKCLKySdgHI3Hj-LifogVVMXQDj9eiT9x3TigxyLYMCYTQ-94zeM8lnPVYCp4FIh7PamWEt7akzyW2fT2xINDqNH4ZtKs45B2SxTA0CXGU4Xvp9YiyFtV9_09ezbaBi2hWwteEOTwZ6MQbgda8hvG9xNPgcTYuG_GRyDjc-urrOXk7KReVVic9SrO3jmNuSrZdb5vcmanQ5KXykmbDQgzDQTR_hYgQe5XwPsRL-cutjIGwyS1bq_DBCgKVqpkmu5JVIuBBkSQPGWn9_T1qTA1XsZAeqoP77hTIFyK2JDr3_NOrw-MoqZHIRdu5ESd_AOFYFaiP2ZJD1u3PMSnCOQmJjXjfJZ0Kjh22Ho_CVQdhzY-vLf-eeR97Rc8QtvpppFr2GqOrgzUDenfbDJfNDfE3TRFr7s=w415-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/pod_pejzazem.jpg" />
+                        <img src="https://lh3.googleusercontent.com/ckLrfDLO9Z_kvzrNjtYeLnId-Mt6J1WJuqxTLSSo3vUZZLwdAbDE_fn6QmBzhDuUnicT6mhiR2Nu_vZi8BUof0Sag0x4BTVjXRH4VEuFP4-eXSEIcoqPBnFupMfUQdTAQ1lgl1tcT7AZv1eiKLOb9OGfgy7FUz3ICHSD2BkDKP9-RkqJFsauWOgUBNN7UBFDJ9m0vLOnfu5xNzHmX2QfhJTR7JjAWFhIzUk-aibhyP-y0Sy3ntXf-rfSGCr_7Tk1h_CGK4ruuV8xkcsOv82T3CDtkaIipov8mUu8-U-EiqNt51gOd08OpyRd6TL0XsPeuNbQF0V5HW9aferGZNk1AXveqTfVTtNyVerVTzJJdaaG7BvSZkauNI1Jwty3T8d4bZjV1SR1j3Y9H-2oywhN0poZLzeXRTHGCXMtaOZEQ-O1RufXDseQENuJQCq4o7Zz5PCCi1N_3ScTNq2u5gY0JQC_sFXTpBVwR-uhRI-KFzfeWPgcXSlMxnH0bzQnoyQGYCixTajG8ZjEBv8-UCNHKvMJjvriykJd3C02xuj3Dvz0t6tzZ-VHNxz7NGmIYJDTIL65fo-rXKFfb-UKAOgrKnR92y2hfb2Af2_sNWiVbPBEy9GLsGZdHg6wDM1drl4dHXYQsmkd9KKCyfKum7-2NQv_fYyA9R-s=w609-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
                         <div>
-                        <img src="./img/paintings/podwojne_zycie_2.jpg" />
+                        <img src="https://lh3.googleusercontent.com/GAG3SeJJgnxBXF1Bv20Sn7MuNg83qs9I_kpx--P9lTrzx7yJL1oTsycp5asAoiz0wch9AAbz-9wKBE8dGO-7iSoVYMzEi6rLYTN6EQ8L-xDMgqzJV2ZcbKlNChXyoZLc7RuBLf1fvhN0OCiqcFD_ttX1gw6u8wOkqlyyIQxZ9SGNNdxfcDeH-a94NsBPQr3e-kArftHgXJOxiP1s3kFlQrhhchIVMHrgA7OSbR65W1wxh03IK2oK239c65xtWwhY9MN31uaeJFI4fA3qKRQ4x-8eBeK5KqIxechJtF8B1tBvhAErZ9DYRWo9uJF5Y6363ff7Pfon9fxSetTwexMKiOlK5-S6Df2V3oHvos9dQeRvvmfdyom5equa69r7dOkXnt3y2pbuAvkE8CERX04M8L33F-3vK02aRmzRX4SIGHVrTw7JENn_hNXXqj2qdoi9yGS1apUQyLjJ3KOmpIxNozFm4yRyBHILZDHTc1EvMUt_OOgSDbPnFItvvKu8gngKn5IOyWPy3X6c5Bq49WT99WkNbv1zoY0J7DPV0T98HZXSt7oRaw3UuIXKhjve4xTk5L_XASGQGCtXinDp1b7MeywyRHfJrSzVVx4DGZKRabCHxi3r2Kv-IcXdTY7E32ydoCkSE77m5NUwmVassuwfhjVlDrEyp4cs=w555-h500-no" />
+                        <h4>Lorem ipsum (1410)</h4>
+                        <br/>
+                        </div>
+                        <div>
+                        <img src="https://lh3.googleusercontent.com/RM4lTLgE8rBEHNxNMLfP1p6kFh-IsQ1vh4qeON7xpyxi3jw_Bx08yhfocj4rB-HqHbyf8zYZcM1YxhePxl0IjWSDH4zGH40m2Yy0M4uRVo8R8BsGI0tJQwphZR1ae1KLx_Eel2pOmv087ReKqmUwKYuJErREvDhEHf7sDHMaOSgwUhZZEGiCw4aT3Cvz4q1P5N5dZTP9CWRQz1I4ZCqxelqyWp30Dn6VkWzdtBd-uYQ1o1caYZr5tXPz1UBosHdtjbktGV4wrZXAXI1kS_LuzcaiSwpnrI8JV5lEkaCoAguDNQwqN5l4bGY6TMGy8m4hiWOYwCLgxj4gueGv_b9vwYgqO40_m6X_eI3_M3u2NbNxByDZHugsGdtAy3UhJvmdLmn2PL5aD-ZFCuWVhgl4HXecA41Kgfcb8llbYnQFUenz5wQnnLL6eu0A54VJDQCZGHShqcKLClkJkDWPDIJy_GIP6UDfMiPdUuxcV6khDOyom9Tzja3VfJcE1T0-GhWhAnoLnEYuI_Yct7B6LCtQ-mpGpiBHXpgWIjc-nNsxe2eo6IoFW2lttPYtFkicYdqo9BdjLWxOfcjtcGpRM_wuieGs7MAFCEEMoIaf0EAlsCjazJoFnTlk1-yMCCc1cUjSzLc1pwHPX2OxDmHIn3oGIrusTKVvdYlE=w575-h500-no" />
+                        <h4>Lorem ipsum (1410)</h4>
+                        <br/>
+                        </div>
+                        <div>
+                        <img src="https://lh3.googleusercontent.com/_IIxM5XcZCsjom8HnXNFV3yc7rRheGRuVyV2LyX4mg5FLWG2kw8Av5Cz-v_EU1MDNNFUnycHzcR0KAWosE64cKHOC_dYA7Q3LsmrG8Pk4xFaapvja3FyaCr-0Sbcitm9O6k012Hi9Uc01JjmozeWL8mAVxEEFPMlL97QQ3z18rNvLTLmDS5TtSfMyN-fpB4IcqdglJsbiphcStB5lm9O3TuIUprckl45XitlQZcJv2Yro6ijIQ4nD2G5zcEhz0mTh-xDCqUifa9CaC4HJNbJ-uzYzR9EtZSJy6cPhY65nfwiNeKuNt2nRPnE7-8H-vLJQYdVh3ghGotyQlkXCTc_-iBMjurLqcVKiee_PLBz1TJlD0c4Fx8gylto__1wdNyKE_8kSroK37mI5uSqbJu0AxUro5vua3v2-e93tnhcfRYlJYkmv1tFwSoNgt4_jB87206RNsBp8htzu3ljkVZRyIaoBH_f1nobUhkOK6AF_wgmjOE-a0V0WQMdLDZb764dS0ImIN_guZZq2mFEfoqTrpVkEFLjUmXQR8u-Pd_2beHmYMp3ucCH7TZYIbA7-QU6vPZ4TTtyYek5SJEJ_3ARW7ZSdqxP1WW1CREWgxzdglPifHCo6NuXT_2iTqD1YjELahqATbWe9L494rbtuXIpK-fU8Ltf-ecR=w602-h500-no" />
+                        <h4>Lorem ipsum (1410)</h4>
+                        <br/>
+                        </div>
+                        <div>
+                        <img src="https://lh3.googleusercontent.com/RumFhLC6htrYKkHZoc_kLE5tNdi1WDCa_DJ6YkK9JGWk9f3uZesyFssUSusHZQBfTQBZNwrypq5aRckCoXZAW_Y9tli13ipy2ntymaUUFZrLYxYwUM4CUhWeJ6wT0lr0B6bJ4Xk7LMiOFLjXOxasADWeUzXzrlTB1V_a83InhoAud0xcMrphfQlEtLZUM651qlvBW11ZFIzHJfgazRYpkmJG2uPl80r72COVFuF9VJMGqxHVfehBUW4LANTYuLMEE6qSZ9iiDntMrCnJTMygmAnay_Nu8Vpf0EeJytRgAnmvudh0ThLF06byWpR5r8gnmzanM-PsYzjRauhm1GoVBUJP6MC_PGtKGYA-NLGerYE8gWUTxHmDdU8SGHCtad9pzVeqXGEtWvDuVE7HGyutn0gc_OQWbZr-YBMoY_xzTOKsAowHQSNH6jF8ITp6v2aRnCqH5FC5wSe3rV-lHG2dtmmz4vMB3GtnDiqntsBvki9hz1u1EjlCU1606nc9dcBPLI0QxizAFJPGS_kgBaZ7rVsYmus0dkDMghKxxiKdtHZ0TNtmq1FH3eyGaiVYXpShKMZ7tWIfarPjwNyHQq7bH3_G-Dk8LqxfvPhokDJoORm3mbg_LrqCYqNTpAjScxZfLdNWHpgY-WtMwM9X0VObQem0lVUpEvtH=w557-h500-no" />
                         <h4>Lorem ipsum (1410)</h4>
                         <br/>
                         </div>
